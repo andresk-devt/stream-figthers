754d94f212f10ef7d3d1602cc8be548a7aa9a692
andresk-devt <learningxandres@gmail.com>
2023-06-02 13:21:29 -0500

    Layout footer and organice the z-index of the x icon in menu sidebar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../assets/styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer-message">Stream figthers © 2023 Todos los derechos reservados.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
