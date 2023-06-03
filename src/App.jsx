65843482e2e20ae057e65c29dcef9b73a11fe5ed
andresk-devt <learningxandres@gmail.com>
2023-06-03 09:48:33 -0500

    Do a good practices with a layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
 import { Routes, Route } from "react-router-dom";
 import Inicio from "./views/Inicio";
-import Peleadores from "./views/Peleadores"
+import Peleadores from "./views/Peleadores";
 import Contenido from "./views/Contenido";
 import Predicciones from "./views/Predicciones";
 import Eventos from "./views/Eventos";
-
+import Layout from "./views/Layout";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route element={<Inicio/>} path="/"></Route>
-        <Route element={<Peleadores/>} path="/peleadores"></Route>
-        <Route element={<Contenido/>} path="/contenido"></Route>
-        <Route element={<Predicciones/>} path="/predicciones"></Route>
-        <Route element={<Eventos/>} path="/eventos"></Route>
+        <Route element={<Layout/>} path="/">
+          <Route element={<Inicio />} path="/"></Route>
+          <Route element={<Peleadores />} path="/peleadores"></Route>
+          <Route element={<Contenido />} path="/contenido"></Route>
+          <Route element={<Predicciones />} path="/predicciones"></Route>
+          <Route element={<Eventos />} path="/eventos"></Route>
+        </Route>
       </Routes>
-      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
