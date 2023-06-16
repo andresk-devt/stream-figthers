e1a504140a9f9652798a63f802ff1ef20ce191ed
andresk-devt <learningxandres@gmail.com>
2023-06-16 14:26:27 -0500

    Figthers card and view organiced

--- src/components/FigtherInformation.jsx
+++ src/components/FigtherInformation.jsx
@@ -1,8 +1,76 @@
-// eslint-disable-next-line react/prop-types
-const FigtherInformation = ({ category, image, name, streak }) => {
+/* eslint-disable react/prop-types */
+import "../assets/styles/FigthsInformationCard.css";
+import { Link } from "react-router-dom";
+
+const FigtherInformation = ({
+  category,
+  weigth,
+  image,
+  name,
+  streak,
+  nickname,
+}) => {
   return (
     <>
-      <div className="figther-card-container">
+      <div className="figther-card">
+        <div className="figther-weigth-column">
+          <p className="category-text">{category}</p>
+          <span className="weigth-text">{weigth}</span>
+        </div>
+        <div className="figther-image">
+          <img src={image} alt="" />
+        </div>
+        <div className="figther-information">
+          <div className="figther-weigth">
+            <p className="category-text">{category}</p>
+            <span className="weigth-text">{weigth}</span>
+          </div>
+          <div className="personal-information">
+            {nickname && (
+              <p className="nickname-text">&quot;{nickname}&quot;</p>
+            )}
+            <p className="name-text">{name}</p>
+            <span className="streak-text">{streak}</span>
+          </div>
+          <div className="figther-social-medias">
+            <button className="follow-button">
+              <p className="button-text">SEGUIR</p>
+              <div className="social-media">
+                <Link>
+                  <box-icon type="logo" name="instagram" size="sm"></box-icon>
+                </Link>
+                <Link>
+                  <box-icon name="twitter" type="logo" size="sm"></box-icon>
+                </Link>
+                <Link>
+                  <box-icon name="twitch" type="logo" size="sm"></box-icon>
+                </Link>
+                <Link>
+                  <box-icon name="youtube" type="logo" size="sm"></box-icon>
+                </Link>
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className="figther-social-media-column">
+          <button className="follow-button">
+            <p className="button-text">SEGUIR</p>
+            <div className="social-media">
+              <Link>
+                <box-icon type="logo" name="instagram" size="sm"></box-icon>
+              </Link>
+              <Link>
+                <box-icon name="twitter" type="logo" size="sm"></box-icon>
+              </Link>
+              <Link>
+                <box-icon name="twitch" type="logo" size="sm"></box-icon>
+              </Link>
+              <Link>
+                <box-icon name="youtube" type="logo" size="sm"></box-icon>
+              </Link>
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
--- src/views/Peleadores.jsx
+++ src/views/Peleadores.jsx
@@ -1,6 +1,7 @@
 import FigtherInformation from "../components/FigtherInformation";
 import figther from "../assets/images/fighter.png";
 import "../assets/styles/FigthsInformationCard.css"
+import womanFigther from "../assets/images/woman-figther.png";
 
 const Peleadores = () => {
   return (
@@ -9,9 +10,27 @@ const Peleadores = () => {
         <h1 className="figthers-tittle">Peladores</h1>
         <FigtherInformation
           category="peso mosca"
+          weigth="125 lb (56.82kg)"
           image={figther}
-          name="ISRAEL ADENSAYA"
+          name="Leandro loaiza"
+          streak="0-1-0 (W-L-D)"
+          nickname="La Sapa"
+        />
+        <FigtherInformation
+          category="peso ballena"
+          weigth="300 lb (136kg)"
+          image={womanFigther}
+          name="Saray Guzmán"
+          streak="1-0-0 (W-L-D)"
+          nickname="Saygus"
+        />
+        <FigtherInformation
+          category="peso mosca"
+          weigth="125 lb (56.82kg)"
+          image={figther}
+          name="Leandro loaiza"
           streak="1-1-0 (W-L-D)"
+          nickname="La Sapa"
         />
       </div>
     </>
